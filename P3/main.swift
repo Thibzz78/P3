1c858e228919c5d8ffc50b8c92ea904ead79efd2
moi <thibault.bernard108@hotmail.fr>
2020-11-24 16:51:41 +0100

    Bug "createTeam"

--- P3/main.swift
+++ P3/main.swift
@@ -15,4 +15,9 @@ var team2 = [Character(lifePoint: 0, attackPoint: 0, name: "Unknow", canHeal: tr
              Character(lifePoint: 0, attackPoint: 0, name: "Unknow", canHeal: true, healPoint: 0, arme: Weapon(weapon1: 0)),
              Character(lifePoint: 0, attackPoint: 0, name: "Unknow", canHeal: true, healPoint: 0, arme: Weapon(weapon1: 0))
             ]
-team1[0].createTeam()
+/*
+team1.createTeam()
+*/
+
+
+
